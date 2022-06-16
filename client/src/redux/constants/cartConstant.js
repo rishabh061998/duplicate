633a539e780ce6efa019dd0835472466d4e9220b
rishabh061998 <rishabhjune06.1998@gmail.com>
2022-06-16 18:06:54 +0530

    2

--- client/src/redux/constants/cartConstant.js
+++
@@ -1,8 +0,0 @@
-
-
-
-
-export const ADD_TO_CART="addTocart";
-export const ADD_TO_CART_ERROR="addToCartError";
-export const REMOVE_FROM_CART="removeFromcart";
-export const CART_RESET="cartReset";
